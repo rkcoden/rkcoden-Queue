967acca1c95a267f462af78f2f901b83844d1945
rkcoden <khanra4@farmingdale.edu>
2023-02-08 23:22:32 +0000

    done

--- src/main/java/QueueExercise.java
+++ src/main/java/QueueExercise.java
@@ -37,7 +37,7 @@ public class QueueExercise {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        return new ArrayDeque<String>();
     }
 
     /**
@@ -46,7 +46,7 @@ public class QueueExercise {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -56,7 +56,7 @@ public class QueueExercise {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+        queue.add(item);
     }
 
     /**
@@ -66,7 +66,7 @@ public class QueueExercise {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        return queue.poll();
     }
 
     /**
@@ -76,6 +76,6 @@ public class QueueExercise {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
